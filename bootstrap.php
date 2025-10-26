05531a3f4e144e7690e2fba044ec580678bbf8d0
StewKI <andrijastev4@gmail.com>
2025-10-26 14:54:30 +0100

    bootstraping refactored to support scripts

--- bootstrap.php
+++ bootstrap.php
@@ -19,16 +19,8 @@ $config = new Config($_ENV);
 $container_bindings = require CONFIG_DIR . '/container_bindings.php';
 
 $container = new Container();
+
+$container->set(Config::class, fn() => $config);
 $container_bindings($container);
 
-$router = new Router($container);
-
-$set_routes = require CONFIG_DIR . '/routes.php';
-$set_routes($router);
-
-$app = new App($config, $router, [
-    'uri' => $_SERVER['REQUEST_URI'],
-    'method' => $_SERVER['REQUEST_METHOD'],
-]);
-
-return $app;
+return $container;
--- public/index.php
+++ public/index.php
@@ -2,11 +2,26 @@
 
 declare(strict_types=1);
 
-/** @var App $app */
+/** @var ContainerInterface $container */
 
-use app\App;
+use App\App;
+use App\Config;
+use App\Router;
+use Psr\Container\ContainerInterface;
 
-$app = require __DIR__ . '/../bootstrap.php';
+$container = require __DIR__ . '/../bootstrap.php';
+
+$router = new Router($container);
+
+$set_routes = require CONFIG_DIR . '/routes.php';
+$set_routes($router);
+
+$config = $container->get(Config::class);
+
+$app = new App($config, $router, [
+    'uri' => $_SERVER['REQUEST_URI'],
+    'method' => $_SERVER['REQUEST_METHOD'],
+]);
 
 $app->boot();
 
